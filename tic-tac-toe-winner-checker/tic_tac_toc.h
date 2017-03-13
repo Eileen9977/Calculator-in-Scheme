5148428a78f7f9c0cb99a886cfcf2a3cf41816ac
Eileen Feng <efeng@brynmawr.edu>
2017-03-13 10:06:28 -0400

    Create tic_tac_toc.h

---
+++ tic-tac-toe-winner-checker/tic_tac_toc.h
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+
+// 
+#define BLANK 0
+#define X     1
+#define O     2
+
+/*  */
+int check_line(int* start, int step, int n);
+
+/* */
+int tic_tac_toe_winner(int n, int* board);
+
